0ab84847f55b5fd9a562e586f82e7c00ed26c5e1
Jaider3 <jaidersuarez89@gmail.com>
2026-05-19 14:57:13 -0500

    Img fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,27 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  base:'/',
+  base: './', // Cambiamos a relativo para que se adapte a subcarpetas
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/imgs', // Toma tus imágenes originales
+          dest: 'pages/ux'    // LAS COPIA DIRECTAMENTE DONDE TU HTML LAS BUSCA
+        },
+        {
+          src: 'public/imgs',
+          dest: 'pages/admin'  // También las copia en admin por si las necesitas ahí
+        }
+      ]
+    })
+  ],
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'pages/ux/index.html'),
+        ux: resolve(__dirname, 'pages/ux/index.html'),
         admin: resolve(__dirname, 'pages/admin/index.html'),
       }
     }
